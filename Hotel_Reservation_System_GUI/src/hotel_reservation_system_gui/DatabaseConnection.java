c7b88df2ef37cd39d51a198661d81636b60c60f1
shershahx <46119@students.riphah.edu.pk>
2024-12-02 14:47:40 +0500

    Add files via upload

---
+++ Hotel_Reservation_System_GUI/src/hotel_reservation_system_gui/DatabaseConnection.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hotel_reservation_system_gui;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/ACP_project";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+
+    // Method to get a database connection
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+}
